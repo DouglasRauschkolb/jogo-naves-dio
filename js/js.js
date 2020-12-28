0673cf2fd4fc0d6ace42d27265271eae700a78a9
DouglasRauschkolb <douglasrauschkolb@yahoo.com.br>
2020-12-28 20:32:43 -0300

    Adicionado limitaçao do movimento do helicoptero 1

--- js/js.js
+++ js/js.js
@@ -52,14 +52,20 @@ function start() { // Inicio da função start()
 	
         if (jogo.pressionou[TECLA.UP]) {
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo-10);
+
+            if (topo>=0) {
+                $("#jogador").css("top",topo-10);
+            }
         
         }
         
         if (jogo.pressionou[TECLA.DOWN]) {
-            
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo+10);	
+
+            if (topo<=434) {	
+                $("#jogador").css("top",topo+10);
+                    
+            }
         }
         
         if (jogo.pressionou[TECLA.RIGHT]) {
