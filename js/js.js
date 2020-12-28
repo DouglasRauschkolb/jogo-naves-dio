71f13d8a599322a2ce67ed26b06a67799dc617c0
DouglasRauschkolb <douglasrauschkolb@yahoo.com.br>
2020-12-28 20:19:05 -0300

    Adicionado funçao principal do jogo e movimentaçao do fundo

--- js/js.js
+++ js/js.js
@@ -5,6 +5,29 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='jogador' class='anima1' ></div>");
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo' class='anima3' ></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3' ></div>");
+    
+    //Principais variáveis do jogo
+	
+	var jogo = {}
+	
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	    movefundo();
+	
+	}
+
+    //Função que movimenta o fundo do jogo
+	
+	function movefundo() {
+	
+	    esquerda = parseInt($("#fundoGame").css("background-position"));
+	    $("#fundoGame").css("background-position",esquerda-1);
+	
+	} 
 
 } // Fim da função start
